633ec158ccacc644a9b501d822cf239eb339db40
alfar0m30 <romeo.fallberg@elev.ga.ntig.se>
2025-01-21 09:38:17 +0100

    Frågesport

---
+++ script.js
@@ -0,0 +1,43 @@
+const länder = ["Holland", "Belgien", "Italien", "Rumänien", "Albanien", "Tjeckien", "Polen", "Österrike"];
+const städer = ["Amsterdam", "Bryssel", "Rom", "Bukarest", "Tirana", "Prag", "Warzawa", "Österrike"];
+
+let p_fråga = document.querySelector("#fråga");
+let input_svar=document.querySelector("#svar");
+let p_resultat=document.querySelector("#resultat");
+
+function fråga(){
+    slumptal = parseInt(Math.random() * länder.length)
+    let fråga = länder[slumptal]
+    p_fråga.textContent = "Ange huvudstad för "+fråga
+    input_svar.value = ""
+}
+
+let points = 0;
+
+function ange_svar(){
+    let svar = input_svar.value;
+    p_resultat.textContent = "Rätt svar: "+städer[slumptal];
+
+    input_svar.value="";
+
+    if (svar.toLowerCase() === städer[slumptal].toLowerCase()) {
+        points++;
+        p_resultat.textContent = "Rätt! Poäng = " + points;
+    } else {
+        p_resultat.textContent = "Fel! Rätt svar var " + städer[slumptal] + ". Poäng = " + points;
+    }
+
+    fråga();
+}
+
+function starta_om(){
+    points = 0;
+    p_resultat.textContent = "Poäng = 0"
+}
+
+input_svar.addEventListener("keydown", (event) =>{
+    if (event.key === "Enter"){
+        ange_svar();
+    }
+}
+)
